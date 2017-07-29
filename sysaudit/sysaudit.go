f98b839492db936fa4e8bc14a3c0c1a79a45f84c
Brent Longborough <brent@longborough.org>
2017-07-29 09:24:01 +0100

    More developments

---
+++ sysaudit/sysaudit.go
@@ -0,0 +1,17 @@
+// sysaudit: Access ALCS system audit data 
+package sysaudit
+
+import (
+	_ "encoding/hex"
+	_ "fmt"
+	_ "math"
+	_ "strings"
+)
+
+const version string = "1.0.0"
+
+const Server string = "10.11.75.214"
+const PgUser string = "datalex"
+const PgPw   string = "datalex"
+const DbName string = "datalex"
+
