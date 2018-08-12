f35b5425c260bd3ac5a0896397e748c2b0297f52
Brent Longborough <brent@longborough.org>
2018-08-12 19:40:02 +0100

    keygen initial tinkering

--- testkg/testkg.go
+++ testkg/testkg.go
@@ -2,17 +2,12 @@
 package main
 
 import (
-	"bytes"
 	"flag"
-	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
-	"syscall"
 
 	"github.com/cloudflare/gokey"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 var (
@@ -20,16 +15,6 @@ var (
 	unsafe                                 bool
 	seedSkipCount                          int
 )
-
-func init() {
-	flag.StringVar(&pass, "p", "", "master password (if not specified, will be asked interactively)")
-	flag.StringVar(&keyType, "t", "pass", "output type (can be pass, seed, raw, ec256, ec521, rsa2048, rsa4096)")
-	flag.StringVar(&seedPath, "s", "", "path to master seed file (optional)")
-	flag.IntVar(&seedSkipCount, "skip", 0, "number of bytes to skip from master seed file (default 0)")
-	flag.StringVar(&realm, "r", "", "password/key realm (most probably purpose of the password/key)")
-	flag.StringVar(&output, "o", "", "output path to store generated key/password (default stdout)")
-	flag.BoolVar(&unsafe, "u", false, "UNSAFE: allow key generation without a seed")
-}
 
 var keyTypes = map[string]gokey.KeyType{
 	"ec256":   gokey.EC256,
@@ -50,18 +35,12 @@ func genSeed(w io.Writer) {
 	}
 }
 
-func genPass(seed []byte, w io.Writer) {
-	password, err := gokey.GetPass(pass, realm, seed, &gokey.PasswordSpec{8, 2, 3, 2, 1, "\"!+%&*-"})
+func genPass(seed []byte) string {
+	password, err := gokey.GetPass(pass, realm, seed, &gokey.PasswordSpec{16, 2, 3, 2, 0, ""})
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	_, err = io.WriteString(w, password)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Fprintln(w, "")
+	return password
 }
 
 func genKey(seed []byte, w io.Writer) {
@@ -97,79 +76,11 @@ func logFatal(format string, args ...interface{}) {
 }
 
 func main() {
-	flag.Parse()
 
-	var err error
-	if pass == "" {
-		var passBytes []byte
-		var passBytesAgain []byte
-		for {
-			for len(passBytes) == 0 {
-				fmt.Print("Master password: ")
-				passBytes, err = terminal.ReadPassword(int(syscall.Stdin))
-				if err != nil {
-					log.Fatalln(err)
-				}
-				fmt.Println("")
-			}
+	//  var pass = "qwerty"
+	//	var realm = "llwyd-consulting.cymru"
+	var seed []byte
 
-			fmt.Print("Master password again: ")
-			passBytesAgain, err = terminal.ReadPassword(int(syscall.Stdin))
-			if err != nil {
-				log.Fatalln(err)
-			}
-			fmt.Println("")
-			if bytes.Equal(passBytes, passBytesAgain) {
-				break
-			} else {
-				fmt.Println("Passwords do not match. Try again.")
-				passBytes = nil
-				continue
-			}
-		}
-
-		pass = string(passBytes)
-	}
-
-	out := os.Stdout
-	if output != "" {
-		out, err = os.OpenFile(output, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer out.Close()
-	}
-
-	if keyType == "seed" {
-		genSeed(out)
-	} else {
-		if realm == "" {
-			logFatal("no realm provided")
-		}
-
-		var seed []byte
-		if seedPath != "" {
-			seed, err = ioutil.ReadFile(seedPath)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			if (seedSkipCount < 0) || (seedSkipCount >= len(seed)) {
-				log.Fatalln("invalid skip parameter")
-			}
-			seed = seed[seedSkipCount:]
-		}
-
-		switch keyType {
-		case "pass":
-			genPass(seed, out)
-		case "raw":
-			genRaw(seed, out)
-		default:
-			if _, ok := keyTypes[keyType]; !ok {
-				logFatal("unknown key type: %v", keyType)
-			}
-			genKey(seed, out)
-		}
-	}
+	var key = genPass(seed)
+	println(key)
 }
