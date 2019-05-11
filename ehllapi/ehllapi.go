7c03890237389c6bc89c361ea872f9c835ac3e06
Brent Longborough <brent@llwyd-consulting.cymru>
2019-05-11 11:15:38 +0100

    New tests with 32-bit go

--- ehllapi/ehllapi.go
+++ ehllapi/ehllapi.go
@@ -1,14 +1,14 @@
-// ehllapi: An interface to IBM and other 3270 terminal emulators.
+// Package ehllapi: An interface to IBM and other 3270 terminal emulators.
 package ehllapi
 
 import (
 	"encoding/binary"
 	"errors"
-	"fmt" 
+	"fmt"
+	"regexp"
+	"strings"
 	"syscall"
 	"unsafe"
-    "strings"
-	"regexp"
 )
 
 const version string = "0.1.0"
@@ -31,87 +31,95 @@ const (
 )
 
 const (
-	dllName     string = `ehlapi32.dll`
-	dllProc     string = "HLLAPI"
+	dllName string = `ehlapi32.dll`
+	dllProc string = "HLLAPI"
 )
 
-var ( reSysId,
-	  reKeys      *regexp.Regexp )
-var ( keytable    map[string]string )
+var (
+	reSysId,
+	reKeys *regexp.Regexp
+)
+var (
+	keytable map[string]string
+)
 
 // Session: a 3270 session handle
 type Session struct {
-	SessId     string
-	SysId      string
-	Entry      string
-	Result     []string
-	Headers    int
-	Footers    int
-	Rows       int
-	Cols       int
-	Scroll     int
-	Proc       *syscall.LazyProc
+	SessId  string
+	SysId   string
+	Entry   string
+	Result  []string
+	Headers int
+	Footers int
+	Rows    int
+	Cols    int
+	Scroll  int
+	Proc    *syscall.LazyProc
 }
 
 func init() {
 	reSysId = regexp.MustCompile("ALCS VTAM application name \\.\\.\\.\\. +([A-Z0-9]{1,8}) ")
-	keytable = map[string]string {
+	keytable = map[string]string{
 		"{tab}":   "@T",
-		"{enter}": "@E", 
-	}  
+		"{enter}": "@E",
+	}
 }
 
 // String: is the interface for printing Session structures
 func (s Session) String() string {
-	return fmt.Sprintf("%1s: %s",s.SessId,s.SysId) 
+	return fmt.Sprintf("%1s: %s", s.SessId, s.SysId)
 }
 
-// NewSession: sets up a Session and connects with its ID 
+// NewSession: sets up a Session and connects with its ID
 func NewSession(sessid string) (Session, error) {
 	if len(sessid) != 1 || sessid < "A" || sessid > "Z" {
 		return Session{}, errors.New("1 Invalid session ID:" + sessid)
 	}
 	sid := strings.ToUpper(sessid)
 	self := Session{
-		SessId:    sid ,
-		SysId:     "UNKNOWN" ,
-		Headers:   0 ,  
-		Footers:   1 ,
-		Rows:      0 ,
-	    Cols:      0 ,
-		Scroll:    0 , 
+		SessId:  sid,
+		SysId:   "UNKNOWN",
+		Headers: 0,
+		Footers: 1,
+		Rows:    0,
+		Cols:    0,
+		Scroll:  0,
 	}
 	return self, nil
-} 
+}
 
 // Connect: connects to a presentation space and determines system ID
 
-func (s Session) Connect() (uint32) {
-// Load EHLLAPI DLL
+func (s Session) Connect() uint32 {
+	// Load EHLLAPI DLL
+	fmt.Println("Start Connect")
 	dll := syscall.NewLazyDLL(dllName)
+	fmt.Println("Connect 1")
 	s.Proc = dll.NewProc(dllProc)
-	
-// Connect to session
-	rc := s.ecall(eConnect,s.SessId,1,0)
+	fmt.Println("Connect 2")
+
+	// Connect to session
+	rc := s.ecall(eConnect, s.SessId, 1, 0)
+	fmt.Println("Connect 3")
 	if rc == 0 {
 		defer s.Disconnect()
 	}
 
-// Get rows and columns
+	// Get rows and columns
 	sessparm := "000000000000000000"[0:18]
-	rc = s.ecall(eQuerySess,sessparm,18,0)
+	rc = s.ecall(eQuerySess, sessparm, 18, 0)
 	s.Rows = int(binary.LittleEndian.Uint16([]byte(sessparm[11:13])))
 	s.Cols = int(binary.LittleEndian.Uint16([]byte(sessparm[13:15])))
 
-// Attempt to discover system name
+	// Attempt to discover system name
 	var eps uint32 = 0
-	var buf = string(make([]byte,s.Rows*s.Cols))
-	rc = s.ecall(eSend,"@C",2,eps)             // Clear screen
-	rc = s.ecall(eWait,buf,1,eps)
+	var buf = string(make([]byte, s.Rows*s.Cols))
+	rc = s.ecall(eSend, "@C", 2, eps) // Clear screen
+	rc = s.ecall(eWait, buf, 1, eps)
 
-	rc = s.ecall(eSend,"zdcom alcs@E",12,eps)  // Query ALCS and read output
-	rc = s.ecall(eWait,buf,1,eps)
-	rc = s.ecall(eReadScrn,buf,uint32(s.Rows*s.Cols),eps)
+	rc = s.ecall(eSend, "zdcom alcs@E", 12, eps) // Query ALCS and read output
+	rc = s.ecall(eWait, buf, 1, eps)
+	rc = s.ecall(eReadScrn, buf, uint32(s.Rows*s.Cols), eps)
 
 	m := reSysId.FindStringSubmatch(buf)
 	if m != nil {
@@ -122,21 +130,21 @@ func (s Session) Connect() (uint32) {
 
 // Disconnect: disconnect from the current presentation space
 
-func (s Session) Disconnect() (uint32) {
-	rc := s.ecall(eDisconnect," ",1,0)
+func (s Session) Disconnect() uint32 {
+	rc := s.ecall(eDisconnect, " ", 1, 0)
 	return rc
 }
 
 // RunEntry: run an entry and capture the output
 
-func (s Session) Write(entry string) (uint32) {
+func (s Session) Write(entry string) uint32 {
 	var rc uint32 = 0
 	return rc
 }
 
 // ecall: provides simplified EHLLAPI DLL calls
 
-func (s Session) ecall(fun ElFunc, buffer string, count uint32, ps uint32) (uint32) {
+func (s Session) ecall(fun ElFunc, buffer string, count uint32, ps uint32) uint32 {
 	rc := ps
 	_, _, _ = s.Proc.Call(
 		uintptr(unsafe.Pointer(uintptr(uint32(fun)))),
